277936bf4019e3b313c13693996c81012755034e
Ashley <adoel3507@gmail.com>
2021-01-18 23:29:21 +0000

    added .forEach and animateFromCurrent

--- public/script/backward_support.js
+++ public/script/backward_support.js
@@ -121,6 +121,66 @@ if (!Array.prototype.findIndex) {
   });
 }
 
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: https://es5.github.io/#x15.4.4.18
+
+if (!Array.prototype['forEach']) {
+
+  Array.prototype.forEach = function(callback, thisArg) {
+
+    if (this == null) { throw new TypeError('Array.prototype.forEach called on null or undefined'); }
+
+    var T, k;
+    // 1. Let O be the result of calling toObject() passing the
+    // |this| value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get() internal
+    // method of O with the argument "length".
+    // 3. Let len be toUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If isCallable(callback) is false, throw a TypeError exception.
+    // See: https://es5.github.com/#x9.11
+    if (typeof callback !== "function") { throw new TypeError(callback + ' is not a function'); }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let
+    // T be undefined.
+    if (arguments.length > 1) { T = thisArg; }
+
+    // 6. Let k be 0
+    k = 0;
+
+    // 7. Repeat, while k < len
+    while (k < len) {
+
+      var kValue;
+
+      // a. Let Pk be ToString(k).
+      //    This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty
+      //    internal method of O with argument Pk.
+      //    This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal
+        // method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Call the Call internal method of callback with T as
+        // the this value and argument list containing kValue, k, and O.
+        callback.call(T, kValue, k, O);
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined
+  };
+}
+
+
 // bug free animation function from HTTP 203 series
 // https://www.youtube.com/watch?v=9-6CKCz58A8
 function animateTo(element, keyframes, options) {
@@ -129,8 +189,29 @@ function animateTo(element, keyframes, options) {
       { ...options, fill: 'both' },
   );
   anim.addEventListener('finish', () => {
-      anim.commitStyles();
-      anim.cancel();
+    anim.commitStyles();
+    anim.cancel();
+    
+      
   });
   return anim
 }
+
+// So you don't have to add the original stats, as some phones required
+function animateFromCurrent(element, keyframes, options) {
+  let newframes = [];
+
+  // For each keyframe, get key name
+  keyframes.some(keyframe => {
+      for (let key in keyframe) {
+
+          // Collate original and new frame, and add in that order
+          let origin = new Object;
+          origin[key] = element.style[key];
+          newframes.push(origin)
+          newframes.push(keyframe);
+
+      } 
+  })
+  return anim = animateTo(element, newframes, options)
+} 
