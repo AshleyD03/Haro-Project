a5fbd21946151ee712118c45d4a638e1f5e95a2b
Ashley <adoel3507@gmail.com>
2021-01-19 21:36:00 +0000

    Added navbar arrow and black-out

--- public/script/navbar.js
+++ public/script/navbar.js
@@ -6,6 +6,10 @@ let nav_summons = document.getElementsByClassName('nav-summon');
 let nav_containers = document.getElementsByClassName('nav-container');
 let nav_cont = document.getElementById('nav-cont');
 let cart_cont = document.getElementById('cart-cont');
+let cart_white = document.getElementById('cart-white');
+let logo_main = document.getElementById('logo-main');
+let logo_arrow = document.getElementById('logo-arrow');
+let nav_blackout = document.getElementById('nav-blackout');
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
 // Initliase bar position and variables
@@ -82,8 +86,12 @@ container.addEventListener('touchmove', e => {
 
                 // Move container and change content opacity
                 container.style.transform = `translateY(${destY - maxY - minY}px)`;
-                document.getElementById(showing).style.opacity = (curY - minY) / (maxY - minY);
+                // Opacity is distanceFraction from top to bot
+                let opacity = (curY - minY) / (maxY - minY) + 0.1;
+                document.getElementById(showing).style.opacity = opacity;
+                logoArrowAppear(opacity, false)
             }
+            // Update Touch
             ongoingTouches[tchIndex] = copyTouch(touch)
 
         } else {
@@ -108,30 +116,29 @@ container.addEventListener('touchend', e => {
             
             // A fraction displaying the percentage of page, from top (0) to bottom (1), that the slider covers
             let disFraction = (curY - minY) / (maxY - minY);
+
             // initialiise animation parts
             let options = {
                 easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)'       
             }
             let keyframes = [] //= [{transform: `translateY(${curY - maxY - minY}px)`}]
+            
 
             // Decide the direction of translation
             let reasonDown = ( !(curY <= maxY * 0.15) && ( (goingDown === true) || (curY >= maxY * 0.85) ));
             if (reasonDown) {
                 // Go down 
-                keyframes.push({transform: `translateY(-${minY}px)`})
-                curY = maxY
                 disFraction = 1 - disFraction; // flip disFraction
+                pushNavbar(minY, maxY, disFraction)
             } else {
                 // Go up
-                keyframes.push({transform: `translateY(-${maxY}px)`})
-                curY = minY
+                pushNavbar(maxY, minY)
             }
 
-            // Duration Equation : y = 450x + 100
-            options['duration'] = 450 * disFraction + 100;
-            animateFromCurrent(container, keyframes, options);
-            ongoingTouches.splice(copyTouch(touch), 1)
-            document.getElementById(showing).style.opacity = (curY - minY) / (maxY - minY);
+            // Opacity is distanceFraction from top to bot
+            let opacity = (curY - minY) / (maxY - minY);
+            document.getElementById(showing).style.opacity = opacity;
+            logoArrowAppear(opacity, (opacity === 1))
         }
     }
   } catch (error) {
@@ -151,13 +158,35 @@ function doesTouchEndOnTarget(touchEvent) {
     else return false
 }
 
-function showAndHide(showing, hiding) {
-    let show = document.getElementById(showing);
-    let hide = document.getElementById(hiding);
-    show.style.pointerEvents = 'auto';
-    hide.style.pointerEvents = 'none';
-    show.style.opacity = 1;
-    hide.style.opacity = 0; 
+
+
+// For swapping logo to arrow
+function logoArrowAppear(opacity=1, pointer=true) {
+    logo_arrow.style.opacity = opacity;
+    nav_blackout.style.opacity = 0.8 * opacity;
+    logo_main.style.opacity = 1 - opacity;
+
+    if (opacity === 0) pointer = false;
+    // Change pointers if true
+    if (pointer === true) {
+        logo_arrow.style.pointerEvents = 'auto';
+        nav_blackout.style.pointerEvents = 'auto';
+        logo_main.style.pointerEvents = 'none';
+    } else {
+        logo_arrow.style.pointerEvents = 'none';
+        nav_blackout.style.pointerEvents = 'none';
+        logo_main.style.pointerEvents = 'auto';
+    }
+}
+
+function pushNavbar(moveY, endY, durationFraction=(curY - minY) / (maxY - minY)) {
+    let options = {
+        easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)',
+        duration: durationFraction * 450 + 100       
+    }
+    let keyframes = [{transform: `translateY(-${moveY}px)`}]
+    animateFromCurrent(container, keyframes, options);
+    curY = endY;
 }
 
 // - = - Navbar Summons - = - 
@@ -168,34 +197,21 @@ Array.from(nav_summons).some(summon => {
 
     // Ignore other touch starts
     summon.addEventListener('touchstart', e => {
-        e.stopPropagation()
+        summon.style.filter = 'brightness(75%)';
     })
 
-    // Let Go event
     summon.addEventListener('touchend', e => {
         e.stopPropagation()
-
-        // Check if correct event to pulldown
-        if (!(doesTouchEndOnTarget(e)) || !reveal) return 
+        summon.style.filter = '';
 
         // Swap option showing / hiding
         if (showing !== reveal) {
             hiding = showing; showing = reveal;
         }      
 
-        // Push up navbar
-        let options = {
-            easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)',
-            duration: (1 - (curY - minY) / (maxY - minY)) * 450 + 100       
-        }
-        let keyframes = [
-            {transform: `translateY(${curY - maxY - minY}px)`}, 
-            {transform: `translateY(-${minY}px)`}
-        ]
-        animateTo(container, keyframes, options);
-        curY = maxY;
-
-        // Swap contents
+        // Push nav down, show arrow and swap
+        pushNavbar(minY, maxY, (1 - (curY - minY) / (maxY - minY)))
+        logoArrowAppear(1)
         showAndHide(showing, hiding)
     })
 })
@@ -205,10 +221,40 @@ Array.from(nav_links).some(link => {
     let href = link.href;
     link.addEventListener('touchstart', e => {
         e.stopPropagation()
+        link.parentElement.style.boxShadow = 'inset -4px -4px 4px rgba(0, 0, 0, 0.25), inset 4px 4px 4px rgba(0, 0, 0, 0.25)';
     })
+
     link.addEventListener('touchend', e => {
         e.stopPropagation()
+        link.parentElement.style.boxShadow = '';
+
         if (!(doesTouchEndOnTarget(e)) || !href) return 
         window.location.href = href;
     })
+})
+
+// Allow normal swipe to occur on cart-white
+cart_white.addEventListener('touchstart', e => e.stopPropagation())
+cart_white.addEventListener('touchmove', e => e.stopPropagation())
+cart_white.addEventListener('touchend', e => e.stopPropagation())
+
+Array.from([nav_blackout, logo_arrow]).forEach(element => {
+
+    // No stop propagation's, as you can press the arrow and scroll at the same time
+    element.addEventListener('touchstart', e => {
+        element.style.filter = 'brightness(50%)';
+    })
+
+    element.addEventListener('touchend', e => {
+        element.style.filter = 'brightness(100%)';
+
+        if (!(doesTouchEndOnTarget(e))) return
+
+        console.log('eee')
+        setTimeout(function() {
+            pushNavbar(maxY, minY, (curY - minY) / (maxY - minY));
+            logoArrowAppear(0)
+        }, 1);
+    })
+
 })
