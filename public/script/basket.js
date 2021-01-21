42a56f399991a745c48f5b20cee8683343c72c6d
Ashley <adoel3507@gmail.com>
2021-01-21 13:07:03 +0000

    created basket controller object

--- public/script/basket.js
+++ public/script/basket.js
@@ -1,61 +1,176 @@
-// Realy good guide, credit due
-// http://www.ashleysheridan.co.uk/blog/Using+Local+Storage+to+Cache+Images
+let basket = (function() {
+    function Basket () {
+        let cart_empty = document.getElementById('cart-empty');
+        let cart_buttons = document.getElementById('cart-buttons');
+        let item_target = document.getElementById('item-target');
+        let template_cart_item = document.getElementById('template-cart-item');
+        let template_item_line = document.getElementById('template-item-line');
+    
+        this.basket;
+    
+        // basket.addItemToBasket('cloth', 'wewdawdw', 'this is description', 69, 69, ['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.barnorama.com%2Fwp-content%2Fuploads%2F2018%2F11%2FBeautiful-And-Sexy-45.jpg&f=1&nofb=1']) 
+        // Add item to basket, used to check right data input
+        this.addItemToBasket = (name, id, description, price, quantity, imageList) => {
+            let copyid = this.basket.findIndex(item => item.id === id)
+    
+            // If the basket already has one in the basket
+            if (copyid !== -1) {
+                this.basket[copyid].quantity += quantity;
+                return this.updateBasket()
+            }
+    
+            // Else add item to basket
+            let item = {
+                name: name,
+                id: id,
+                description: description,
+                price: price,
+                quantity: quantity,
+                imageurl: imageList
+            }
+    
+            this.basket.push(item)
+            return this.updateBasket()
+        }
+    
+        // Lower quantity of item.id by quantity ammmount. If less then 1, remove it.
+        this.takeItemOutBasket = (id, quantity) => {
+            let copyid = this.basket.findIndex(item => item.id === id);
+    
+            if (!copyid) return null
+    
+            // Change quantity or clear if none left
+            this.basket[copyid].quantity -= quantity;
+            if (this.basket[copyid].quanity < 1) this.basket.splice(copyid, 1);
+            return this.updateBasket()
+        }
+    
+        // Remove all of an item
+        this.removeAllOfItem = (id) => {
+            let copyid = this.basket.findIndex(item => item.id === id);
+            this.basket.splice(copyid, 1);
+            return this.updateBasket()
+        }
+    
+    
+        //  Load Basket Visuals for Page
+        this.loadBasket = () => {
+            this.expireOldItems()
+    
+            this.basket = this.getItem('basket');
+            item_target.innerHTML = '';
+    
+            // If basket empty, make empty UI appear
+            if (!this.basket) {
+                this.basket = [];
+                cart_buttons.style.display = 'none';
+                cart_empty.style.display = 'block';
+                return
+            }
+            
+            // Make not-empty UI appear
+            cart_buttons.style.display = 'block';
+            cart_empty.style.display = 'none';
 
-// Stringify data into json string, to then push to storage
-function setItem (key, data) {
-    try {
-        let time = new Date();
-        let json = JSON.stringify({time: time, data: data})
-
-        window.localStorage.setItem(key, json)
-        return true
-    } catch (e) {
-        return false
+            // Loop through basket items
+            this.basket.forEach(item => {
+                let template = template_cart_item.content.cloneNode(true).children[0];
+                
+                // === Mobile Styling
+                let img = template.children[0].children[0];
+                let name = template.children[0].children[1];
+                let price = template.children[1].children[1];
+                let quantity = template.children[2].children[1];
+                let total = template.children[3].children[1];
+    
+                img.src = item.imageurl[0];
+                name.innerHTML = item.name;
+                price.innerHTML = item.price;
+                quantity.innerHTML = item.quantity // Implement quantity
+                total = item.price * item.quantity 
+    
+                item_target.appendChild(template)
+    
+                // Add line element for spacing
+                let line = template_item_line.content.cloneNode(true).children[0];
+                item_target.appendChild(line)
+                // ===
+            })
+    
+            // Remove last line
+            item_target.removeChild(item_target.lastChild);
+        }
+    
+        // Make code easier to read
+        this.updateBasket = () => {
+           this.setItem('basket', this.basket);
+           this.loadBasket();
+        }
+    
+        // Stringify data into json string, to then push to storage
+        this.setItem = (key, data) => {
+            try {
+                let time = new Date();
+                let json = JSON.stringify({time: time, data: data})
+    
+                window.localStorage.setItem(key, json)
+                return true
+            } catch (e) {
+                return false
+            }
+        }
+    
+        // Get json string from storage, and parse back to json
+        this.getItem = (key) => {
+            try {
+                let jsonString = window.localStorage.getItem(key);
+                let parseData = JSON.parse(jsonString);
+    
+                return parseData.data
+            } catch (e) {
+                return null;
+            }
+        }
+    
+        // Check time property of all item's in storage. If they have been in storage for more then a week, remove them.
+        this.expireOldItems = () => {
+            let n = window.localStorage.length;
+            let now = new Date();
+            let maxAge = 1000 * 60 * 60 * 24 * 7 // 7 Days
+    
+            for (let i; i < n; i++) {
+                let itemKey = window.localStorage.key(i);
+                let itemData = window.localStorage.getItem(itemKey)
+    
+                try {
+                    let parseData = JSON.parse(itemData);
+                    let itemTime = new Date(parseData.time);
+                    let timeDif = now.valueOf() - itemTime.valueOf();
+    
+                    if (timeDif > maxAge) window.localStorage.removeItem(itemKey);
+    
+                } catch (e) {}
+            }
+        }
+    
+        // Draw canvas data of image and encode to Data URL to return
+        this.encodeImageToUrl = (image) => {
+            let canvas = document.createElement('canvas');
+            canvas.height = image.height;
+            canvas.width = image.width;
+    
+            let ctx = canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0);
+    
+            let dataUrl = canvas.toDataURL('image/png')
+            console.log(dataUrl)
+            return dataUrl
+        }
+    
+        this.loadBasket();
     }
-}
+    // Realy good guide, credit due
+    // http://www.ashleysheridan.co.uk/blog/Using+Local+Storage+to+Cache+Images
+    return new Basket();
+})()
 
-// Get json string from storage, and parse back to json
-function getItem (key) {
-    try {
-        let jsonString = window.localStorage.getItem(key);
-        let parseData = JSON.parse(jsonString);
-
-        return parseData.data
-    } catch (e) {
-        return null;
-    }
-}
-
-// Check time property of all item's in storage. If they have been in storage for more then a week, remove them.
-function expireOldItems () {
-    let n = window.localStorage.length;
-    let now = new Date();
-    let maxAge = 1000 * 60 * 60 * 24 * 7 // 7 Days
-
-    for (let i; i < n; i++) {
-        let itemKey = window.localStorage.key(i);
-        let itemData = window.localStorage.getItem(itemKey)
-
-        try {
-            let parseData = JSON.parse(itemData);
-            let itemTime = new Date(parseData.time);
-            let timeDif = now.valueOf() - itemTime.valueOf();
-
-            if (timeDif > maxAge) window.localStorage.removeItem(itemKey);
-
-        } catch (e) {}
-    }
-}
-
-// Draw canvas data of image and encode to Data URL to return
-function encodeImageToUrl (image) {
-    let canvas = document.createElement('canvas');
-    canvas.height = image.height;
-    canvas.width = image.width;
-
-    let ctx = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0);
-
-    let dataUrl = canvas.toDataURL('image/png')
-    return dataUrl
-}
--- public/script/navbar.js
+++ public/script/navbar.js
@@ -1,260 +1,264 @@
-let container = document.getElementById('container');
-let hidden = document.getElementById('hidden');
-let bar = document.getElementById('bar');
-let nav_links = document.getElementsByClassName('nav-link');
-let nav_summons = document.getElementsByClassName('nav-summon');
-let nav_containers = document.getElementsByClassName('nav-container');
-let nav_cont = document.getElementById('nav-cont');
-let cart_cont = document.getElementById('cart-cont');
-let cart_white = document.getElementById('cart-white');
-let logo_main = document.getElementById('logo-main');
-let logo_arrow = document.getElementById('logo-arrow');
-let nav_blackout = document.getElementById('nav-blackout');
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+let navbar = (function () {
+    let container = document.getElementById('container');
+    let hidden = document.getElementById('hidden');
+    let bar = document.getElementById('bar');
+    let nav_links = document.getElementsByClassName('nav-link');
+    let nav_summons = document.getElementsByClassName('nav-summon');
+    let nav_containers = document.getElementsByClassName('nav-container');
+    let nav_cont = document.getElementById('nav-cont');
+    let cart_cont = document.getElementById('cart-cont');
+    let cart_white = document.getElementById('cart-white');
+    let logo_main = document.getElementById('logo-main');
+    let logo_arrow = document.getElementById('logo-arrow');
+    let nav_blackout = document.getElementById('nav-blackout');
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
-// Initliase bar position and variables
-let ongoingTouches, minY, maxY, curY, barLock, goingDown, showing, hiding; 
-window.addEventListener('load', initBar())
-window.addEventListener('resize', initBar())
-window.addEventListener('orientationchange', initBar())
+    // Initliase bar position and variables
+    let ongoingTouches, minY, maxY, curY, barLock, goingDown, showing, hiding; 
+    window.addEventListener('load', initBar())
+    window.addEventListener('resize', initBar())
+    window.addEventListener('orientationchange', initBar())
 
-// Move bar and set vairables
-function initBar() {
-    ongoingTouches = []
-    minY = bar.clientHeight;
-    maxY = hidden.clientHeight + parseFloat(window.getComputedStyle(hidden).marginTop);
-    curY = minY;
-    barLock = false;
-    goingDown = false;
-    hiding = 'cart-cont';
-    showing = 'nav-cont';
-    showAndHide(showing, hiding)
-    container.style.transform = `translateY(-${maxY}px)`
-    Array.from(nav_containers).forEach(cont => cont.style.height = hidden.clientHeight);
-    cart_cont.style.transform = `translateY(-${nav_cont.clientHeight}px)`
-}
-
-
-
-// - = - Navbar Swipes - = -
-
-// On touch add new touches to ongoingTouches
-container.addEventListener('touchstart', e => {
-    e.preventDefault();
-    let touches = e.changedTouches;
-    for (var i = 0; i < touches.length; i++) {
-        ongoingTouches.push(copyTouch(touches[i]))
-    }
-    console.log('click')
-}, false)
-
-
-// Move Touch EVENT 
-container.addEventListener('touchmove', e => {
-  try {
-    e.preventDefault();
-    let touches = e.changedTouches;
-
-    if (barLock) return
-
-    for (var i = 0; i < touches.length; i++) {
-
-        let touch = touches[i]
-        let id = touch.identifier;
-        let tchIndex = ongoingTouches.findIndex(tch => tch.identifier === id);
-       
-        // Check that the touch is valid
-        if (tchIndex > -1) {
-
-            // Calculate Destination
-            let dif = ongoingTouches[tchIndex].pageY - touch.pageY
-            let acceleration = Math.abs(dif)
-            dif *= 2
-            let limit = 25;
-            if (Math.abs(dif) > limit) acceleration = limit;
-            let destY = curY - ( acceleration * dif / Math.abs(dif) )
-
-            // If destination is valid
-            if ((destY > minY) && (destY < maxY)) {
-
-                // Update direction and currentY
-                goingDown = false;
-                if (destY > curY) {
-                    goingDown = true;
-                } 
-                curY = destY;
-
-                // Move container and change content opacity
-                container.style.transform = `translateY(${destY - maxY - minY}px)`;
-                // Opacity is distanceFraction from top to bot
-                let opacity = (curY - minY) / (maxY - minY) + 0.1;
-                document.getElementById(showing).style.opacity = opacity;
-                logoArrowAppear(opacity, false)
-            }
-            // Update Touch
-            ongoingTouches[tchIndex] = copyTouch(touch)
-
-        } else {
-            console.log('unverified move')
-        }
-    }
-  } catch (error) {
-    console.error(error)
-  }
-}, false)
-
-// End Touch EVENT
-container.addEventListener('touchend', e => {
-  try {
-    e.preventDefault();
-    let touches = e.changedTouches;
-
-    for (var i = 0; i < touches.length; i++) {
-        let touch = touches[i]
-        let id = touch.identifier;
-        if (ongoingTouches.some(tch => tch.identifier === id)) {
-            
-            // A fraction displaying the percentage of page, from top (0) to bottom (1), that the slider covers
-            let disFraction = (curY - minY) / (maxY - minY);
-
-            // initialiise animation parts
-            let options = {
-                easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)'       
-            }
-            let keyframes = [] //= [{transform: `translateY(${curY - maxY - minY}px)`}]
-            
-
-            // Decide the direction of translation
-            let reasonDown = ( !(curY <= maxY * 0.15) && ( (goingDown === true) || (curY >= maxY * 0.85) ));
-            if (reasonDown) {
-                // Go down 
-                disFraction = 1 - disFraction; // flip disFraction
-                pushNavbar(minY, maxY, disFraction)
-            } else {
-                // Go up
-                pushNavbar(maxY, minY)
-            }
-
-            // Opacity is distanceFraction from top to bot
-            let opacity = (curY - minY) / (maxY - minY);
-            document.getElementById(showing).style.opacity = opacity;
-            logoArrowAppear(opacity, (opacity === 1))
-        }
-    }
-  } catch (error) {
-    console.error(error)
-  }
-}, false)
-
-// copy key touch properties
-function copyTouch({ identifier, pageX, pageY }) {
-    return { identifier, pageX, pageY };
-}
-
-function doesTouchEndOnTarget(touchEvent) {
-    let touches = touchEvent.changedTouches;
-    let checkTarget = touch => (document.elementFromPoint(touch.pageX, touch.pageY) ===  touchEvent.target)
-    if (Array.from(touches).some(checkTarget)) return true
-    else return false
-}
-
-
-
-// For swapping logo to arrow
-function logoArrowAppear(opacity=1, pointer=true) {
-    logo_arrow.style.opacity = opacity;
-    nav_blackout.style.opacity = 0.8 * opacity;
-    logo_main.style.opacity = 1 - opacity;
-
-    if (opacity === 0) pointer = false;
-    // Change pointers if true
-    if (pointer === true) {
-        logo_arrow.style.pointerEvents = 'auto';
-        nav_blackout.style.pointerEvents = 'auto';
-        logo_main.style.pointerEvents = 'none';
-    } else {
-        logo_arrow.style.pointerEvents = 'none';
-        nav_blackout.style.pointerEvents = 'none';
-        logo_main.style.pointerEvents = 'auto';
-    }
-}
-
-function pushNavbar(moveY, endY, durationFraction=(curY - minY) / (maxY - minY)) {
-    let options = {
-        easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)',
-        duration: durationFraction * 450 + 100       
-    }
-    let keyframes = [{transform: `translateY(-${moveY}px)`}]
-    animateFromCurrent(container, keyframes, options);
-    curY = endY;
-}
-
-// - = - Navbar Summons - = - 
-// Do to all .nav_summon
-Array.from(nav_summons).some(summon => {
-
-    let reveal = summon.getAttribute('value')
-
-    // Ignore other touch starts
-    summon.addEventListener('touchstart', e => {
-        summon.style.filter = 'brightness(75%)';
-    })
-
-    summon.addEventListener('touchend', e => {
-        e.stopPropagation()
-        summon.style.filter = '';
-
-        // Swap option showing / hiding
-        if (showing !== reveal) {
-            hiding = showing; showing = reveal;
-        }      
-
-        // Push nav down, show arrow and swap
-        pushNavbar(minY, maxY, (1 - (curY - minY) / (maxY - minY)))
-        logoArrowAppear(1)
+    // Move bar and set vairables
+    function initBar() {
+        ongoingTouches = []
+        minY = bar.clientHeight;
+        maxY = hidden.clientHeight + parseFloat(window.getComputedStyle(hidden).marginTop);
+        curY = minY;
+        barLock = false;
+        goingDown = false;
+        hiding = 'cart-cont';
+        showing = 'nav-cont';
         showAndHide(showing, hiding)
+        container.style.transform = `translateY(-${maxY}px)`
+        Array.from(nav_containers).forEach(cont => cont.style.height = hidden.clientHeight);
+        cart_cont.style.transform = `translateY(-${nav_cont.clientHeight}px)`
+    }
+
+    // - = - Navbar Swipes - = -
+
+    // On touch add new touches to ongoingTouches
+    container.addEventListener('touchstart', e => {
+        e.preventDefault();
+        let touches = e.changedTouches;
+        for (var i = 0; i < touches.length; i++) {
+            ongoingTouches.push(copyTouch(touches[i]))
+        }
+        console.log('click')
+    }, false)
+
+
+    // Move Touch EVENT 
+    container.addEventListener('touchmove', e => {
+    try {
+        e.preventDefault();
+        let touches = e.changedTouches;
+
+        if (barLock) return
+
+        for (var i = 0; i < touches.length; i++) {
+
+            let touch = touches[i]
+            let id = touch.identifier;
+            let tchIndex = ongoingTouches.findIndex(tch => tch.identifier === id);
+        
+            // Check that the touch is valid
+            if (tchIndex > -1) {
+
+                // Calculate Destination
+                let dif = ongoingTouches[tchIndex].pageY - touch.pageY
+                let acceleration = Math.abs(dif)
+                dif *= 2
+                let limit = 25;
+                if (Math.abs(dif) > limit) acceleration = limit;
+                let destY = curY - ( acceleration * dif / Math.abs(dif) )
+
+                // If destination is valid
+                if ((destY > minY) && (destY < maxY)) {
+
+                    // Update direction and currentY
+                    goingDown = false;
+                    if (destY > curY) {
+                        goingDown = true;
+                    } 
+                    curY = destY;
+
+                    // Move container and change content opacity
+                    container.style.transform = `translateY(${destY - maxY - minY}px)`;
+                    // Opacity is distanceFraction from top to bot
+                    let opacity = (curY - minY) / (maxY - minY) + 0.1;
+                    document.getElementById(showing).style.opacity = opacity;
+                    logoArrowAppear(opacity, false)
+                }
+                // Update Touch
+                ongoingTouches[tchIndex] = copyTouch(touch)
+
+            } else {
+                console.log('unverified move')
+            }
+        }
+    } catch (error) {
+        console.error(error)
+    }
+    }, false)
+
+    // End Touch EVENT
+    container.addEventListener('touchend', e => {
+    try {
+        e.preventDefault();
+        let touches = e.changedTouches;
+
+        for (var i = 0; i < touches.length; i++) {
+            let touch = touches[i]
+            let id = touch.identifier;
+            if (ongoingTouches.some(tch => tch.identifier === id)) {
+                
+                // A fraction displaying the percentage of page, from top (0) to bottom (1), that the slider covers
+                let disFraction = (curY - minY) / (maxY - minY);
+
+                // initialiise animation parts
+                let options = {
+                    easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)'       
+                }
+                let keyframes = [] //= [{transform: `translateY(${curY - maxY - minY}px)`}]
+                
+
+                // Decide the direction of translation
+                let reasonDown = ( !(curY <= maxY * 0.15) && ( (goingDown === true) || (curY >= maxY * 0.85) ));
+                if (reasonDown) {
+                    // Go down 
+                    disFraction = 1 - disFraction; // flip disFraction
+                    pushNavbar(minY, maxY, disFraction)
+                } else {
+                    // Go up
+                    pushNavbar(maxY, minY)
+                }
+
+                // Opacity is distanceFraction from top to bot
+                let opacity = (curY - minY) / (maxY - minY);
+                document.getElementById(showing).style.opacity = opacity;
+                logoArrowAppear(opacity, (opacity === 1))
+            }
+        }
+    } catch (error) {
+        console.error(error)
+    }
+    }, false)
+
+    // - = - Navbar Summons - = - 
+    // Do to all .nav_summon
+    Array.from(nav_summons).some(summon => {
+
+        let reveal = summon.getAttribute('value')
+
+        // Ignore other touch starts
+        summon.addEventListener('touchstart', e => {
+            summon.style.filter = 'brightness(75%)';
+        })
+
+        summon.addEventListener('touchend', e => {
+            summon.style.filter = '';
+
+            if (!(doesTouchEndOnTarget(e))) return
+            e.stopPropagation()
+
+            // Swap option showing / hiding
+            if (showing !== reveal) {
+                hiding = showing; showing = reveal;
+            }      
+
+            // Push nav down, show arrow and swap
+            pushNavbar(minY, maxY, (1 - (curY - minY) / (maxY - minY)))
+            logoArrowAppear(1)
+            showAndHide(showing, hiding)
+        })
     })
-})
 
-// Make href's clickable instead of dragable
-Array.from(nav_links).some(link => {
-    let href = link.href;
-    link.addEventListener('touchstart', e => {
-        e.stopPropagation()
-        link.parentElement.style.boxShadow = 'inset -4px -4px 4px rgba(0, 0, 0, 0.25), inset 4px 4px 4px rgba(0, 0, 0, 0.25)';
+    // Make href's clickable instead of dragable
+    Array.from(nav_links).some(link => {
+        let href = link.href;
+        link.addEventListener('touchstart', e => {
+            e.stopPropagation()
+            link.parentElement.style.boxShadow = 'inset -4px -4px 4px rgba(0, 0, 0, 0.25), inset 4px 4px 4px rgba(0, 0, 0, 0.25)';
+        })
+
+        link.addEventListener('touchend', e => {
+            e.stopPropagation()
+            link.parentElement.style.boxShadow = '';
+
+            if (!(doesTouchEndOnTarget(e)) || !href) return 
+            window.location.href = href;
+        })
     })
 
-    link.addEventListener('touchend', e => {
-        e.stopPropagation()
-        link.parentElement.style.boxShadow = '';
+    // Allow normal swipe to occur on cart-white
+    cart_white.addEventListener('touchstart', e => e.stopPropagation())
+    cart_white.addEventListener('touchmove', e => e.stopPropagation())
+    cart_white.addEventListener('touchend', e => e.stopPropagation())
 
-        if (!(doesTouchEndOnTarget(e)) || !href) return 
-        window.location.href = href;
-    })
-})
+    Array.from([nav_blackout, logo_arrow]).forEach(element => {
 
-// Allow normal swipe to occur on cart-white
-cart_white.addEventListener('touchstart', e => e.stopPropagation())
-cart_white.addEventListener('touchmove', e => e.stopPropagation())
-cart_white.addEventListener('touchend', e => e.stopPropagation())
+        // No stop propagation's, as you can press the arrow and scroll at the same time
+        element.addEventListener('touchstart', e => {
+            element.style.filter = 'brightness(50%)';
+        })
 
-Array.from([nav_blackout, logo_arrow]).forEach(element => {
+        element.addEventListener('touchend', e => {
+            element.style.filter = 'brightness(100%)';
 
-    // No stop propagation's, as you can press the arrow and scroll at the same time
-    element.addEventListener('touchstart', e => {
-        element.style.filter = 'brightness(50%)';
-    })
+            if (!(doesTouchEndOnTarget(e))) return
+            e.stopPropagation()
 
-    element.addEventListener('touchend', e => {
-        element.style.filter = 'brightness(100%)';
-
-        if (!(doesTouchEndOnTarget(e))) return
-
-        console.log('eee')
-        setTimeout(function() {
             pushNavbar(maxY, minY, (curY - minY) / (maxY - minY));
             logoArrowAppear(0)
-        }, 1);
+        })
     })
 
-})
+    // copy key touch properties
+    function copyTouch({ identifier, pageX, pageY }) {
+        return { identifier, pageX, pageY };
+    }
+
+    function doesTouchEndOnTarget(touchEvent) {
+        let touches = touchEvent.changedTouches;
+        let checkTarget = touch => (document.elementFromPoint(touch.pageX, touch.pageY) ===  touchEvent.target)
+        if (Array.from(touches).some(checkTarget)) return true
+        else return false
+    }
+
+    // For swapping logo to arrow
+    function logoArrowAppear(opacity=1, pointer=true) {
+        logo_arrow.style.opacity = opacity;
+        nav_blackout.style.opacity = 0.8 * opacity;
+        logo_main.style.opacity = 1 - opacity;
+
+        if (opacity === 0) pointer = false;
+        // Change pointers if true
+        if (pointer === true) {
+            logo_arrow.style.pointerEvents = 'auto';
+            nav_blackout.style.pointerEvents = 'auto';
+            logo_main.style.pointerEvents = 'none';
+        } else {
+            logo_arrow.style.pointerEvents = 'none';
+            nav_blackout.style.pointerEvents = 'none';
+            logo_main.style.pointerEvents = 'auto';
+        }
+    }
+
+    function pushNavbar(moveY, endY, durationFraction=(curY - minY) / (maxY - minY)) {
+        let options = {
+            easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)',
+            duration: durationFraction * 450 + 100
+        }
+        let keyframes = [{transform: `translateY(-${moveY}px)`}]
+        animateFromCurrent(container, keyframes, options);
+        curY = endY;
+    }
+
+    return ({
+        doesTouchEndOnTarget: doesTouchEndOnTarget, 
+        pushNavbar: pushNavbar,
+        minY: minY, 
+        maxY: maxY
+    })
+})()
