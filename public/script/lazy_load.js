9003a9b1b127326acaee4709db9b40bf80201eae
Ashley <adoel3507@gmail.com>
2021-01-26 00:40:08 +0000

    created lazy load function

---
+++ public/script/lazy_load.js
@@ -0,0 +1,36 @@
+(function() {
+    let lazy_images = Array.from(document.getElementsByClassName('lazy-load'));
+
+    let checkImages = () => {
+
+        lazy_images.forEach(img => {
+            
+            // Check if about to scroll on
+            let scrollTop = window.pageYOffset;
+
+            if (img.offsetTop < (window.innerHeight + scrollTop)) {
+
+                // change src to data-src and remove from list
+                img.src = img.dataset.src;
+                lazy_images.splice(lazy_images.indexOf(img), 1);
+
+            }
+
+            if (lazy_images.length === 0) {
+
+                // Remove event listeners   
+                document.removeEventListener('scroll', checkImages);
+                window.removeEventListener('resize', checkImages);
+                window.removeEventListener('orientationchange', checkImages);
+
+            }
+        })
+
+    }
+
+    checkImages();
+
+    document.addEventListener('scroll', checkImages);
+    window.addEventListener('resize', checkImages);
+    window.addEventListener('orientationChange', checkImages);
+})()
