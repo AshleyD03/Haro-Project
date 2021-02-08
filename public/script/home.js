990f6e426a9694a17389ba9fdcddec21eab86dd2
Ashley <adoel3507@gmail.com>
2021-02-08 00:47:11 +0000

    Added swipe controlls

--- public/script/home.js
+++ public/script/home.js
@@ -1,1 +1,19 @@
-let slideshow = new Slideshow('slideshow')
+let ss_container = document.getElementById('slideshow-container');
+let ss_controlls = Array.from(document.getElementById('slideshow-controlls').children);
+
+ss_container.addEventListener('moveTo', e => {
+    console.log(e)
+    let target = e.detail.target
+    ss_controlls.forEach(element => element.style.background = 'rgba(51, 51, 51, 0.42)')
+    ss_controlls[target].style.background = 'rgba(51, 51, 51, 0.75)';
+})
+
+let slideShow = new Slideshow('slideshow-container', document.getElementById('mob-container'));
+
+for (let i = 0; i < ss_controlls.length; i++) {
+    let cont = ss_controlls[i];
+    console.log(cont)
+    cont.addEventListener('click', e => {
+        slideShow.moveTo(i);
+    })
+}
--- public/script/slideshow.js
+++ public/script/slideshow.js
@@ -1,7 +1,11 @@
 // New slideshow object
-function Slideshow(target, options=null) {
-    let getWidth = () => this.cont.clientWidth / this.slides.length
-  
+function Slideshow(target, widthTarget=null, options=null) {
+    let getWidth = () =>  {
+        if (!widthTarget) return (this.cont.clientWidth / this.slides.length)
+        else return widthTarget.clientWidth;
+    }
+
+
     // Properties
     this.cont = document.getElementById(target);
     this.pos = 0;
@@ -12,21 +16,26 @@ function Slideshow(target, options=null) {
         easing: 'cubic-bezier(0.39, 0.575, 0.565, 1)',
         duration: 1000
     };
+
+    // Apply styling changes
+    // Background Colour
+    let firstColour = window.getComputedStyle(this.slides[1], null).getPropertyValue('background-color') || window.getComputedStyle(this.cont, null).getPropertyValue('background-color');
+    let lastColour = window.getComputedStyle(this.slides[this.slides.length-1], null).getPropertyValue('background-color');
+    this.slides[0].style.backgroundColor = firstColour;
+    this.cont.parentNode.style.backgroundColor = lastColour;
   
     // Move slideshow view port to show slides array target pos
     this.moveTo = (target, duration=null) => {
+        target += 1
+
+        // Change container's background colour to prevent animation clipping
+        let oldColour = this.cont.parentNode.style.backgroundColor;
+        this.cont.parentNode.style.backgroundColor = this.slides[target].style.backgroundColor;
 
         // Check if target input valid 
-        if (target !== 0 && !target || target - 1 > this.slides.length || typeof target !== 'number' || target % 1 !== 0) {
+        if (target !== 0 && !target || target - 2 > this.slides.length || typeof target !== 'number' || target % 1 !== 0) {
             return console.log(`Invalid target for slide : ${target}`)    
         }
-  
-        // If already there
-        if (target === this.pos) return true
-  
-
-        
-        console.log('moveto')
 
         // Create settings for transfer
         let width = this.width;
@@ -39,8 +48,20 @@ function Slideshow(target, options=null) {
   
         // Animate and change pos property
         let anim = animateTo(this.cont, keyframes, options)
-        this.pos = target;
+        console.log('moveto')
+        this.pos = target - 1;
         this.curX = width * target;
+    
+        this.cont.dispatchEvent(new CustomEvent('moveTo', {
+            detail: {
+                name: 'moveTo',
+                slideshow: this,
+                animation: anim,
+                slides: this.slides,
+                target: target-1
+            }
+        }));
+
         return true
     }
 
@@ -74,6 +95,7 @@ function Slideshow(target, options=null) {
                 // Apply change in direction
                 let change = touch.pageX - oldTouch.pageX
 
+                
                 this.curX = Math.round(this.curX - change);  
                 this.cont.style.transform = `translateX(-${this.curX}px)`;
 
@@ -103,36 +125,44 @@ function Slideshow(target, options=null) {
                 ongoingTouches.splice(index, 1)
 
                 // Work out current pos and target in array unit
-                let pos = this.curX / this.width;
-                let ratio = pos % 1;
-                let target;
+                let pos = this.curX / this.width - 1; 
+                let ratio = -Math.abs((pos % 1) - 1) + 1;
+                let duration = Math.abs(ratio * this.options.duration);
+                if (pos < this.curX);
+                let target = this.pos;
 
                 let goRight = false;
                 if (oldTouch.distance < 0) goRight = true;
 
                 // Work out target in array
-                if (this.curX < 0) {
-
+                if (this.curX < this.width) {
                     // Swipe too far left
                     target = 0;
 
                 } else 
-                if (this.curX > this.slides.length * this.width) {
-
+                if (this.curX > (this.slides.length - 1) * this.width) {
                     // Swipe too far right
-                    target = this.slides.length - 1;
+                    target = this.slides.length - 2;
 
-                } else 
-                if (ratio > 0.2 &&) {
+                } else
+                if (ratio < 0.15) {
+                    // Swipe isn't far enough
+                    target = this.pos;
+                    duration = 100;
+
+                } else
+                if (pos < this.pos) {
+                    // Swipe is far enough to go left
+                    target = this.pos - 1
+                } else {
+                    // Swipe is far enough to go right
+                    target = this.pos + 1
+                    duration = Math.abs(-duration + this.options.duration)
 
                 }
 
-                // Work out duration with target
-
                 //this.moveTo(target, duration)
-                console.log('pos', pos, 'ratio', ratio, 'target', target, 'duration', duration, 'go right', goRight)
-                
-                this.curX = target
+                this.moveTo(target, duration)
             }
         }
     })
@@ -141,14 +171,18 @@ function Slideshow(target, options=null) {
     // To adjust transformation with window resize
     let onResize = e => {
         this.width = getWidth()
-        this.curX = this.width * this.pos;
+        this.curX = this.width * (this.pos + 1);
         this.cont.style.transform = `translateX(-${this.curX}px)`;
+        // Width of slides (account for scrollbar)
+        this.slides.forEach(e => e.style.width = `${this.width}px`)
     }
     window.addEventListener('resize', onResize)
+    onResize()
 
     // Copy Touch Function
     function copyTouch({ identifier, pageX, distance=0 }) {
         return { identifier, pageX, distance };
     }
 
+    this.moveTo(0)
   }
